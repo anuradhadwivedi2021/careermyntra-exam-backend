35db851c42e7da37fdd001f818c8b806eeede91c
Anuradha Dwivedi <anuradha.dwivedi2021@gmail.com>
2026-09-05 13:28:08 +0530

    updated files

--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -5,6 +5,7 @@ const { verifyAdmin } = require('../middleware/auth');
 
 router.post('/', verifyAdmin, examController.createExam);
 router.get('/', examController.listExams);
+router.get('/admin/all', verifyAdmin, examController.listAllExamsForAdmin);
 router.get('/:exam_id', examController.getExamById);
 
 module.exports = router;
